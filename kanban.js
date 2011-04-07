ee8e242b19821e7444364fb634f0b18d7d57037e
Alessandro Nadalin <alessandro.nadalin@gmail.com>
2011-04-07 02:37:07 +0200

    some tryouts :)

--- kanban.js
+++ kanban.js
@@ -1,9 +1,41 @@
-var Http = require('http');
+var sys = require('sys');
+var Fs   = require('fs');
+var Url = require('url');
+var Querystring = require('querystring');
+var http = require('http');
+var events = require('events');
+var listener = function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    sys.puts('Started connection');
+## SOCKET.io!
+    Fs.readFile('/home/odino/projects/kanbanjs/frontend/kanban.html', function (err, data) {
+        if (err) throw err;
+        res.write(data);
+        server.on('request', function (request, response) {
+          this.setMaxListeners(0);
+          qs = Url.parse(req.url).query;
+              res.write('s');
+              res.write('<script type="text/javascript">$( "#draggable" ).animate({"left": "' + 200 + 'px"}, 1);</script>');
+              res.write('<script type="text/javascript">$( "#draggable" ).animate({"top": "' + 200 + 'px"}, 1);</script>');
 
-Http.createServer(function (req, res) {
+          if (qs)
+          {
+              qss = Querystring.parse(qs);
+              console.log(qss);
+              res.write('s');
+              res.write('<script type="text/javascript">$( "#draggable" ).animate({"left": "' + 200 + 'px"}, 1);</script>');
+              res.write('<script type="text/javascript">$( "#draggable" ).animate({"top": "' + 200 + 'px"}, 1);</script>');
+              response.end();
+          }
 
-res.write("Hello");
-res.write();
+        });
+    });
+};
 
-}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
+
+
+var server = http.createServer(listener);
+
+console.log();
+
+server.listen(8124);
