6ad34f199b3d633686fd55c3c5ff217d82f3e60a
Alessandro Nadalin <alessandro.nadalin@gmail.com>
2011-04-10 13:24:44 +0200

    concurrent setup done

--- kanban.js
+++ kanban.js
@@ -1,35 +1,45 @@
-var http = require('http'),  
-io = require('/home/odino/www/kanbanjs/socketio');
-var Fs   = require('fs');
+var http = require('http');
+var io = require('./socketio');
+var fs   = require('fs');
+var url = require('url');
 
 var clients = [];
 
-server = http.createServer(function(req, res){ 
- // your normal server code 
- Fs.readFile('/home/odino/www/kanbanjs/frontend/kanban.html', function (err, data) {
-        if (err) throw err;
-        res.write(data);
-	res.end();
-        console.log('connet');
-    });
-
+server = http.createServer(function(req, res, sockable){ 
+    if(url.parse(req.url).pathname == '/socketio.js') {
+       fs.readFile('./socketio/support/socket.io-client/socket.io.js', function (err, data) {
+            if (err) throw err;
+            res.write(data);
+	        res.end();
+        });
+    } else {
+        fs.readFile('./frontend/kanban.html', function (err, data) {
+            if (err) throw err;
+            res.write(data);
+	        res.end();
+        });
+    }
 
 });
-server.listen(8124);
+var port = 8124;
+server.listen(port);
+console.log('server listening on port ' + port);
+
   
 // socket.io 
 var socket = io.listen(server); 
 
-
-
 socket.on('connection', function(client){ 
   clients.push(client);
+
   client.on('message', function(mess){ 
-    clients.forEach(function(client){
-     client.send('grazie di avermelo detto');
-    })
+
+    for (cid in clients)
+    {
+      console.log('activerid:' + mess.rid);
+      clients[cid].send(mess);
+    };
   }) 
   client.on('disconnect', function(){ console.log('B') }) 
   console.log(clients.length);
 }); 
-
